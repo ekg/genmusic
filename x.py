de624c4fcf2f0950574449e91ff0894616982120
Erik Garrison <erik.garrison@gmail.com>
2023-06-22 09:31:06 +0200

    ok

--- x.py
+++ x.py
@@ -5,9 +5,20 @@ from audiocraft.data.audio import audio_write
 model = MusicGen.get_pretrained('large')
 model.set_generation_params(duration=8)  # generate 8 seconds.
 
-descriptions = ['happy rock', 'energetic EDM', 'sad jazz']
+# read descriptions file name as the first argument
+descriptions_file = sys.argv[1]
 
-wav = model.generate(descriptions)  # generates 3 samples.
+# take descriptions as input
+# descriptions are separated by a blank line in the text file
+descriptions = []
+with open(descriptions_file, 'r') as f:
+    descriptions = f.read().split('\n\n')
+
+#descriptions = ['happy rock', 'energetic EDM', 'sad jazz']
+print(descriptions)
+
+# generate audio
+wav = model.generate(descriptions)  # generates N samples
 
 for idx, one_wav in enumerate(wav):
     # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
